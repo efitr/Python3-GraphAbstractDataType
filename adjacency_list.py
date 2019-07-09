65a1d67cbe9d17c68b3d3f7377fcd8f570d81a72
Egon Fiedler <egonfiedler@Egons-MacBook-Air.local>
2019-07-09 04:47:00 -0700

    Probably this must be done with a LinkedList

--- adjacency_list.py
+++ adjacency_list.py
@@ -1,1 +1,7 @@
 # Adjacency List DataStructure
+from linkedlist import LinkedList, Node
+class AdjacencyList(object):
+
+    def __init__(self, init_size):
+        self.nodes = [LinkedList() for i in range(init_size)
+        
