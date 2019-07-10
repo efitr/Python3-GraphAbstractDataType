c66637decb115fd067f747cb9b0ff55473c4686d
Egon Fiedler <egonfiedler@Egons-MacBook-Air.local>
2019-07-09 19:05:37 -0700

    Improved variable naming

--- adjacency_list.py
+++ adjacency_list.py
@@ -5,5 +5,14 @@ class AdjacencyList(object):
         self.id = key
         self.linked_to = dict()
 
-    
-        
+    def add_connection(self, another_element, weight=0):
+        self.linked_to[another_element] = weight
+
+    def get_connections(self):
+        return self.linked_to.keys()
+
+    def get_id(self):
+        return self.id
+
+    def get_weight(self, another_element):
+        return self.linked_to[another_element]    
