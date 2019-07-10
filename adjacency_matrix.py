df8cd62235d72cf8f7398d72ee64e6a9b959af9f
Egon Fiedler <egonfiedler@Egons-MacBook-Air.local>
2019-07-09 19:36:49 -0700

    Sort of done with the AdjacencyMatrix

--- adjacency_matrix.py
+++ adjacency_matrix.py
@@ -1,7 +1,21 @@
 # Adjacency Matrix DataStructure
-from numpy import ndarray
 
 class AdjacencyMatrix(object):
 
-    def __init__(self, init_data):
-        self.matrix = np.array([[], []])
+    def __init__(self, size):
+        self.matrix = []
+        for index in range(size):
+            self.matrix.append([0 for index in range(size)])
+        self.size = size
+
+    def add_connection(self, index_1, index_2):
+        if index_1 == index_2:
+            return 
+        self.matrix[index_1][index_2] = 1
+        self.matrix[index_2][index_1] = 1
+
+    def remove_edge(self, index_1, index_2):
+        if self.matrix[index_1][index_2] == 0:
+            return
+        self.matrix[index_1][index_2] = 0
+        self.matrix[index_2][index_1] = 0
