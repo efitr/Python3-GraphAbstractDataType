12070e978f53cb4b846fd132c4167b982a5c35cf
Egon Fiedler <egonfiedler@Egons-MacBook-Air.local>
2019-07-09 18:52:48 -0700

    Done with the initializer

--- adjacency_list.py
+++ adjacency_list.py
@@ -1,7 +1,9 @@
 # Adjacency List DataStructure
-from linkedlist import LinkedList, Node
 class AdjacencyList(object):
 
-    def __init__(self, init_size):
-        self.nodes = [LinkedList() for i in range(init_size)
+    def __init__(self, key):
+        self.id = key
+        self.linked_to = dict()
+
+    
         
