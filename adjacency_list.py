b368530c1fb08742419f611287824315a6344140
Egon Fiedler <egonfiedler@Egons-MacBook-Air.local>
2019-07-10 20:18:50 -0700

    What to do next

--- adjacency_list.py
+++ adjacency_list.py
@@ -1,4 +1,11 @@
 # Adjacency List DataStructure
+## This is a basic DataStructure that probably has adaptations for all types of graphs.
+### Make all possible possibilities, in this case 4?.
+#### Combination1: For Undirected and Unweighted Graph
+#### Combination2: For Directed and Unweighted Graph 
+#### Combination3: For Undirected and Weighted Graph
+#### Combination4: For Directed and Weighted Graph
+
 class AdjacencyList(object):
 
     def __init__(self, key):
